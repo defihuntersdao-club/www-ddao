94d018b402d090fb6d878126db42e1f34fddf14b
Liksagen.com <nmetauo@gmail.com>
2022-01-31 20:21:20 +0000

    index

--- htdocs/index.php
+++ htdocs/index.php
@@ -366,19 +366,6 @@
 			<div class="language">DDAO available in: <a href="#">русский</a></div>
 		</div>
 	</section>
-
-
-	
-	
-
-
-	
-
-
-
-	
-
-
 	<script src="js/jquery-3.5.1.min.js"></script>
 	<script src="js/script.js"></script>
 
