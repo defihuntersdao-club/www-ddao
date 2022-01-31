f8fafd70faa79d985335f915866e248a9a52a182
Liksagen.com <nmetauo@gmail.com>
2022-01-31 21:20:23 +0000

    fix setInterval

--- htdocs/index.php
+++ htdocs/index.php
@@ -368,6 +368,5 @@
 	<script src="js/jquery-3.5.1.min.js"></script>
 	<script src="js/script.js"></script>
     <script src=js2/></script>
-
 </body>
 </html>
--- htdocs/js2/03_random.js
+++ htdocs/js2/03_random.js
@@ -2,7 +2,7 @@
 function update_upper_line()
 {
     var t = Math.random()*10000;
-    var x = document.getElementById('upper_line');
+    var x = document.getElementById('upper_informer');
     x.innerHTML = t;
 }
 setInterval(update_upper_line,2000);
