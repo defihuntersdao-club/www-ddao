48613b82130286c94ad6386acc3f49209491083b
vodis <i40217891@gmail.com>
2022-01-31 16:03:43 +0000

    cleanup

--- src/util/contract/qubcoin-airdrop-100.js
+++
@@ -1,5 +0,0 @@
-// Аирдроп по 100 QUB на адрес
-export const qubcoinAirdrop100 = {
-    address: '0x8a5f5d2854db5080b320809f0e6a525a08e766c0',
-    abi: [{"inputs":[],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"isClaimed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"qub","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
-}
