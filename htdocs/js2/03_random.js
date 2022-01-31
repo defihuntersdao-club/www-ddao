4556189a9bad256f49db045c6bd3eed93b0d460b
Liksagen.com <nmetauo@gmail.com>
2022-01-31 21:32:14 +0000

    random in upper_informer

--- htdocs/js2/03_random.js
+++ htdocs/js2/03_random.js
@@ -1,7 +1,7 @@
 
 function update_upper_line()
 {
-    var t = Math.random()*10000;
+    var t = Math.random()*1000000000;
     var x = document.getElementById('upper_informer');
     x.innerHTML = t;
 
