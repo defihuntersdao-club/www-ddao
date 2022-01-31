8b14b9b5b6f6d3e90a12112cff23add3b5f7b6bf
Liksagen.com <nmetauo@gmail.com>
2022-01-31 20:14:02 +0000

    add static index

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,15 +3,13 @@
 
 <head>
 
-<!-- thsone -->
-
 	<meta charset="utf-8">
 	<title>DEFI HANTERS DAO</title>
 	<meta name="description" content="DEFI HANTERS DAO">
 	<meta name="viewport" content="width=device-width">
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
 	
     <link rel="stylesheet" href="css/fonts.css">
 	<link rel="stylesheet" href="css/style.css">
@@ -23,24 +21,6 @@
 
 <body class="dark-theme">
 	<header>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-		<h1>Hello</h1>
-
 		<div class="top-info">This project is beta. DYOR</div>
 		<div class="top-header d-flex">
 			<div class="top-header__logo">
@@ -61,8 +41,15 @@
 					<span>Polygon</span>
 				</a>
 				<div class="top-header__wallet">
-					<a href="#" class="top-header__wallet-link">Connect Wallet</a>
+					<div class="top-header__wallet-bl">
+						<div class="top-header__wallet-m">100.78 MATIC</div>
+						<div class="top-header__wallet-id">
+							0хС60В...9се8
+							<span class="status"></span>
+						</div>
+					</div>
 				</div>
+				
 				
 				<div class="menu-block">
 					<a href="#" class="menu-block__btn">
@@ -130,12 +117,12 @@
 
 					<div class="top-views__wrap d-flex">
 						<div class="views d-flex">
-							<a href="#" class="views__link grid active">
+							<a href="#" class="views__link grid ">
 								<svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M1.80078 6.50586H4.80078C5.35078 6.50586 5.80078 6.05586 5.80078 5.50586V1.50586C5.80078 0.955859 5.35078 0.505859 4.80078 0.505859H1.80078C1.25078 0.505859 0.800781 0.955859 0.800781 1.50586V5.50586C0.800781 6.05586 1.25078 6.50586 1.80078 6.50586ZM1.80078 13.5059H4.80078C5.35078 13.5059 5.80078 13.0559 5.80078 12.5059V8.50586C5.80078 7.95586 5.35078 7.50586 4.80078 7.50586H1.80078C1.25078 7.50586 0.800781 7.95586 0.800781 8.50586V12.5059C0.800781 13.0559 1.25078 13.5059 1.80078 13.5059ZM7.80078 13.5059H10.8008C11.3508 13.5059 11.8008 13.0559 11.8008 12.5059V8.50586C11.8008 7.95586 11.3508 7.50586 10.8008 7.50586H7.80078C7.25078 7.50586 6.80078 7.95586 6.80078 8.50586V12.5059C6.80078 13.0559 7.25078 13.5059 7.80078 13.5059ZM13.8008 13.5059H16.8008C17.3508 13.5059 17.8008 13.0559 17.8008 12.5059V8.50586C17.8008 7.95586 17.3508 7.50586 16.8008 7.50586H13.8008C13.2508 7.50586 12.8008 7.95586 12.8008 8.50586V12.5059C12.8008 13.0559 13.2508 13.5059 13.8008 13.5059ZM7.80078 6.50586H10.8008C11.3508 6.50586 11.8008 6.05586 11.8008 5.50586V1.50586C11.8008 0.955859 11.3508 0.505859 10.8008 0.505859H7.80078C7.25078 0.505859 6.80078 0.955859 6.80078 1.50586V5.50586C6.80078 6.05586 7.25078 6.50586 7.80078 6.50586ZM12.8008 1.50586V5.50586C12.8008 6.05586 13.2508 6.50586 13.8008 6.50586H16.8008C17.3508 6.50586 17.8008 6.05586 17.8008 5.50586V1.50586C17.8008 0.955859 17.3508 0.505859 16.8008 0.505859H13.8008C13.2508 0.505859 12.8008 0.955859 12.8008 1.50586Z" fill="#BABDCE"/>
 									</svg>
 							</a>
-							<a href="#" class="views__link line">
+							<a href="#" class="views__link line active">
 								<svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<path d="M1.13672 9.00586H3.13672C3.68672 9.00586 4.13672 8.55586 4.13672 8.00586V6.00586C4.13672 5.45586 3.68672 5.00586 3.13672 5.00586H1.13672C0.586719 5.00586 0.136719 5.45586 0.136719 6.00586V8.00586C0.136719 8.55586 0.586719 9.00586 1.13672 9.00586ZM1.13672 14.0059H3.13672C3.68672 14.0059 4.13672 13.5559 4.13672 13.0059V11.0059C4.13672 10.4559 3.68672 10.0059 3.13672 10.0059H1.13672C0.586719 10.0059 0.136719 10.4559 0.136719 11.0059V13.0059C0.136719 13.5559 0.586719 14.0059 1.13672 14.0059ZM1.13672 4.00586H3.13672C3.68672 4.00586 4.13672 3.55586 4.13672 3.00586V1.00586C4.13672 0.455859 3.68672 0.00585938 3.13672 0.00585938H1.13672C0.586719 0.00585938 0.136719 0.455859 0.136719 1.00586V3.00586C0.136719 3.55586 0.586719 4.00586 1.13672 4.00586ZM6.13672 9.00586H16.1367C16.6867 9.00586 17.1367 8.55586 17.1367 8.00586V6.00586C17.1367 5.45586 16.6867 5.00586 16.1367 5.00586H6.13672C5.58672 5.00586 5.13672 5.45586 5.13672 6.00586V8.00586C5.13672 8.55586 5.58672 9.00586 6.13672 9.00586ZM6.13672 14.0059H16.1367C16.6867 14.0059 17.1367 13.5559 17.1367 13.0059V11.0059C17.1367 10.4559 16.6867 10.0059 16.1367 10.0059H6.13672C5.58672 10.0059 5.13672 10.4559 5.13672 11.0059V13.0059C5.13672 13.5559 5.58672 14.0059 6.13672 14.0059ZM5.13672 1.00586V3.00586C5.13672 3.55586 5.58672 4.00586 6.13672 4.00586H16.1367C16.6867 4.00586 17.1367 3.55586 17.1367 3.00586V1.00586C17.1367 0.455859 16.6867 0.00585938 16.1367 0.00585938H6.13672C5.58672 0.00585938 5.13672 0.455859 5.13672 1.00586Z" fill="#BABDCE"/>
 									</svg>
@@ -164,11 +151,215 @@
 				</div>
 			</div>
 
-			<div class="wrapper bg-white">
-				<div class="connect-bl">
-					<img src="images/disk.svg" alt="" />
-					<p>Your positions will appear here</p>
-					<a href="#" class="art-button">Connect a wallet</a>
+			<div class="wrapper">
+				<div class="line-wrap">
+					<div class="line-item">
+						<div class="line-item__top d-flex">
+							<div class="line-item__title">
+								Seed
+							</div>
+							<div class="line-item__amount">
+								<div class="label">
+									Your DDAO amount
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__claimed">
+								<div class="label">
+									Already Claimed
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__details">
+								<span>Detail</span>
+								<svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M1.16016 1.42017L6.00097 6.26099L10.8418 1.42017" stroke="#3D8AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									</svg>
+									
+							</div>
+						</div>
+						<div class="line-item__bottom d-flex">
+							<div class="line-item__button">
+								<a href="#" class="art-button-2">
+									Claim
+								</a>
+							</div>
+							<div class="line-item__info">
+								<div>
+									<div class="label">Contract address:</div>
+									<div class="value">xxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+								</div>
+								<div>
+									<div class="label">DDAO on contract:</div>
+									<div class="value">xxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+								</div>
+								<div>
+									<div class="label">Date:</div>
+									<div class="value">Jan. 1, 2022</div>
+								</div>
+							</div>
+
+							<div class="line-item__block">
+								<div class="line-item__links">
+									<a href="#">
+										<span>Medium</span>
+										<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M18.7598 19.2494H6.75977C6.20977 19.2494 5.75977 18.7994 5.75977 18.2494V6.24939C5.75977 5.69939 6.20977 5.24939 6.75977 5.24939H11.7598C12.3098 5.24939 12.7598 4.79939 12.7598 4.24939C12.7598 3.69939 12.3098 3.24939 11.7598 3.24939H5.75977C4.64977 3.24939 3.75977 4.14939 3.75977 5.24939V19.2494C3.75977 20.3494 4.65977 21.2494 5.75977 21.2494H19.7598C20.8598 21.2494 21.7598 20.3494 21.7598 19.2494V13.2494C21.7598 12.6994 21.3098 12.2494 20.7598 12.2494C20.2098 12.2494 19.7598 12.6994 19.7598 13.2494V18.2494C19.7598 18.7994 19.3098 19.2494 18.7598 19.2494ZM14.7598 4.24939C14.7598 4.79939 15.2098 5.24939 15.7598 5.24939H18.3498L9.21977 14.3794C8.82977 14.7694 8.82977 15.3994 9.21977 15.7894C9.60977 16.1794 10.2398 16.1794 10.6298 15.7894L19.7598 6.65939V9.24939C19.7598 9.79939 20.2098 10.2494 20.7598 10.2494C21.3098 10.2494 21.7598 9.79939 21.7598 9.24939V4.24939C21.7598 3.69939 21.3098 3.24939 20.7598 3.24939H15.7598C15.2098 3.24939 14.7598 3.69939 14.7598 4.24939Z" fill="#3D8AFF"/>
+											</svg>
+											
+									</a>
+								</div>
+								<div class="line-item__button">
+									<a href="#" class="art-button-2">
+										Claim
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="line-item">
+						<div class="line-item__top d-flex">
+							<div class="line-item__title">
+								Seed
+							</div>
+							<div class="line-item__amount">
+								<div class="label">
+									Your DDAO amount
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__claimed">
+								<div class="label">
+									Already Claimed
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__details">
+								<span>Detail</span>
+								<svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M1.16016 1.42017L6.00097 6.26099L10.8418 1.42017" stroke="#3D8AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									</svg>
+									
+							</div>
+						</div>
+						<div class="line-item__bottom d-flex">
+							<div class="line-item__button">
+								<a href="#" class="art-button-2">
+									Claim
+								</a>
+							</div>
+							<div class="line-item__info">
+								<div>
+									<div class="label">Contract address:</div>
+									<div class="value">xxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+								</div>
+								<div>
+									<div class="label">DDAO on contract:</div>
+									<div class="value">xxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+								</div>
+								<div>
+									<div class="label">Date:</div>
+									<div class="value">Jan. 1, 2022</div>
+								</div>
+							</div>
+
+							<div class="line-item__block">
+								<div class="line-item__links">
+									<a href="#">
+										<span>Medium</span>
+										<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M18.7598 19.2494H6.75977C6.20977 19.2494 5.75977 18.7994 5.75977 18.2494V6.24939C5.75977 5.69939 6.20977 5.24939 6.75977 5.24939H11.7598C12.3098 5.24939 12.7598 4.79939 12.7598 4.24939C12.7598 3.69939 12.3098 3.24939 11.7598 3.24939H5.75977C4.64977 3.24939 3.75977 4.14939 3.75977 5.24939V19.2494C3.75977 20.3494 4.65977 21.2494 5.75977 21.2494H19.7598C20.8598 21.2494 21.7598 20.3494 21.7598 19.2494V13.2494C21.7598 12.6994 21.3098 12.2494 20.7598 12.2494C20.2098 12.2494 19.7598 12.6994 19.7598 13.2494V18.2494C19.7598 18.7994 19.3098 19.2494 18.7598 19.2494ZM14.7598 4.24939C14.7598 4.79939 15.2098 5.24939 15.7598 5.24939H18.3498L9.21977 14.3794C8.82977 14.7694 8.82977 15.3994 9.21977 15.7894C9.60977 16.1794 10.2398 16.1794 10.6298 15.7894L19.7598 6.65939V9.24939C19.7598 9.79939 20.2098 10.2494 20.7598 10.2494C21.3098 10.2494 21.7598 9.79939 21.7598 9.24939V4.24939C21.7598 3.69939 21.3098 3.24939 20.7598 3.24939H15.7598C15.2098 3.24939 14.7598 3.69939 14.7598 4.24939Z" fill="#3D8AFF"/>
+											</svg>
+											
+									</a>
+								</div>
+								<div class="line-item__button">
+									<a href="#" class="art-button-2">
+										Claim
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="line-item coming-soon">
+						<div class="line-item__top d-flex">
+							<div class="line-item__title">
+								Seed
+								<span>cooming soon</span>
+							</div>
+							<div class="line-item__amount">
+								<div class="label">
+									Your balance
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__claimed">
+								<div class="label">
+									Already Claimed
+								</div>
+								<div class="value">
+									0.0
+								</div>
+							</div>
+							<div class="line-item__details">
+								<span>Detail</span>
+								<svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M1.16016 1.42017L6.00097 6.26099L10.8418 1.42017" stroke="#3D8AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+									</svg>
+									
+							</div>
+						</div>
+						<div class="line-item__bottom d-flex">
+							<div class="line-item__button">
+								<a href="#" class="art-button-2">
+									Claim
+								</a>
+							</div>
+							<div class="line-item__info">
+								
+								<div>
+									<div class="label">Date:</div>
+									<div class="value">Jan. 1, 2022</div>
+								</div>
+							</div>
+
+							<div class="line-item__block">
+								<div class="line-item__links">
+									<a href="#">
+										<span>Farming on GYSR</span>
+										<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M18.7598 19.2494H6.75977C6.20977 19.2494 5.75977 18.7994 5.75977 18.2494V6.24939C5.75977 5.69939 6.20977 5.24939 6.75977 5.24939H11.7598C12.3098 5.24939 12.7598 4.79939 12.7598 4.24939C12.7598 3.69939 12.3098 3.24939 11.7598 3.24939H5.75977C4.64977 3.24939 3.75977 4.14939 3.75977 5.24939V19.2494C3.75977 20.3494 4.65977 21.2494 5.75977 21.2494H19.7598C20.8598 21.2494 21.7598 20.3494 21.7598 19.2494V13.2494C21.7598 12.6994 21.3098 12.2494 20.7598 12.2494C20.2098 12.2494 19.7598 12.6994 19.7598 13.2494V18.2494C19.7598 18.7994 19.3098 19.2494 18.7598 19.2494ZM14.7598 4.24939C14.7598 4.79939 15.2098 5.24939 15.7598 5.24939H18.3498L9.21977 14.3794C8.82977 14.7694 8.82977 15.3994 9.21977 15.7894C9.60977 16.1794 10.2398 16.1794 10.6298 15.7894L19.7598 6.65939V9.24939C19.7598 9.79939 20.2098 10.2494 20.7598 10.2494C21.3098 10.2494 21.7598 9.79939 21.7598 9.24939V4.24939C21.7598 3.69939 21.3098 3.24939 20.7598 3.24939H15.7598C15.2098 3.24939 14.7598 3.69939 14.7598 4.24939Z" fill="#3D8AFF"/>
+											</svg>
+											
+									</a>
+									<a href="#">
+										<span>Medium</span>
+										<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+											<path d="M18.7598 19.2494H6.75977C6.20977 19.2494 5.75977 18.7994 5.75977 18.2494V6.24939C5.75977 5.69939 6.20977 5.24939 6.75977 5.24939H11.7598C12.3098 5.24939 12.7598 4.79939 12.7598 4.24939C12.7598 3.69939 12.3098 3.24939 11.7598 3.24939H5.75977C4.64977 3.24939 3.75977 4.14939 3.75977 5.24939V19.2494C3.75977 20.3494 4.65977 21.2494 5.75977 21.2494H19.7598C20.8598 21.2494 21.7598 20.3494 21.7598 19.2494V13.2494C21.7598 12.6994 21.3098 12.2494 20.7598 12.2494C20.2098 12.2494 19.7598 12.6994 19.7598 13.2494V18.2494C19.7598 18.7994 19.3098 19.2494 18.7598 19.2494ZM14.7598 4.24939C14.7598 4.79939 15.2098 5.24939 15.7598 5.24939H18.3498L9.21977 14.3794C8.82977 14.7694 8.82977 15.3994 9.21977 15.7894C9.60977 16.1794 10.2398 16.1794 10.6298 15.7894L19.7598 6.65939V9.24939C19.7598 9.79939 20.2098 10.2494 20.7598 10.2494C21.3098 10.2494 21.7598 9.79939 21.7598 9.24939V4.24939C21.7598 3.69939 21.3098 3.24939 20.7598 3.24939H15.7598C15.2098 3.24939 14.7598 3.69939 14.7598 4.24939Z" fill="#3D8AFF"/>
+											</svg>
+											
+									</a>
+								</div>
+								<div class="line-item__button">
+									<a href="#" class="art-button-2">
+										Claim
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
